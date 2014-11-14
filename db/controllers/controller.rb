dcdfd9c476f1366f0bbcbf31289ce2253e0516c8
Sharmila Jesupaul <tosharmila@gmail.com>
2014-11-14 13:27:36 -0800

    added new controller functions, implemented tweet length checks and created an array of 1000 ideas

--- db/controllers/controller.rb
+++ db/controllers/controller.rb
@@ -1,17 +1,33 @@
 # for testing:
-# require_relative '../../config/config'
+require_relative '../../config/config'
+require 'faker'
 
 class Controller
+  attr_reader :all_ideas
   def initialize
     @companies = Company.all
     @categories = Category.all
     @audiences = Audience.all
+    @all_ideas = []
   end
 
   def rand_idea
     company_name = @companies.sample.name
     audience_name = @audiences.sample.name
-    "It's like #{company_name} for #{audience_name}"
+    idea = "It's like #{company_name}, but for #{audience_name}"
+    store_ideas(idea)
+    idea
+  end
+
+  def tweet_idea
+    company_name = @companies.sample.name
+    audience_name = @audiences.sample.name
+    idea = "It's like #{company_name}, but for #{audience_name}"
+    until tweet_length_check(idea) do
+      idea = "It's like #{company_name}, but for #{audience_name}"
+    end
+    store_ideas(idea)
+    idea
   end
 
   # need to make more associations between companies and categories
@@ -19,16 +35,51 @@ class Controller
     category_obj = Category.where(name: category).first
     company_name = category_obj.companies.sample.name
     audience_name = @audiences.sample.name
-    "It's like #{company_name} for #{audience_name}"
+    idea = "It's like #{company_name} but for #{audience_name}"
+    store_ideas(idea)
+    idea
+  end
+
+  def tweet_by_category(category)
+    category_obj = Category.where(name: category).first
+    company_name = category_obj.companies.sample.name
+    audience_name = @audiences.sample.name
+    idea = "It's like #{company_name} but for #{audience_name}"
+    until tweet_length_check(idea) do
+      idea = "It's like #{company_name}, but for #{audience_name}"
+    end
+    store_ideas(idea)
+    idea
   end
 
   def tweet_length_check(string)
     string.length < 130
   end
+
+  def store_ideas(idea)
+    @all_ideas << idea
+  end
+
 end
 
+controller = Controller.new
+
+1000.times do
+  controller.rand_idea
+  # controller.all_ideas
+end
+
+p controller.all_ideas
 
 
-# new_controller = Controller.new
-# p new_controller.rand_idea
+
+
+
 # p new_controller.idea_by_category('transportation')
+# tweet = new_controller.tweet_idea
+# p tweet
+# p tweet.size
+
+# tweet_category = new_controller.tweet_by_category('transportation')
+# p tweet_category
+# p tweet_category.size
