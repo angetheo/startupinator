b22cc20cf41492a49f253c5d3c548cd1bb3fbdd9
Angelo Tomasco <angelotomasco@gmail.com>
2014-11-13 21:31:18 -0800

    added automated retweeting method

--- twitter/twitter.rb
+++ twitter/twitter.rb
@@ -7,16 +7,27 @@ example_string = "Hello world!"
 
 # Here we have to login with the tokens provided by the Twitter app...
 $client = Twitter::REST::Client.new do |config|
-  config.consumer_key        = "YOUR_CONSUMER_KEY"
-  config.consumer_secret     = "YOUR_CONSUMER_SECRET"
-  config.access_token        = "YOUR_ACCESS_TOKEN"
-  config.access_token_secret = "YOUR_ACCESS_SECRET"
+  config.consumer_key        = "g0sjVBXgdWp3fqx06aHJaAGUC"
+  config.consumer_secret     = "I87ts6cUgXIthPMWjMhTmuNCQunVYhBO2HudXputKmhCJnmoUv"
+  config.access_token        = "2898963229-dE57TZ54LtMLvAuWDx0g3SUkM7HXIaoJWjqwwXn"
+  config.access_token_secret = "xaxTgvh3fV6WT4ftsLcxLAXZ4IBnQXNoMDr7qmI9hMrgl"
 end
 
 def tweet!(text)
-  text.length <= 140 ? $client.update(text) : raise("Error: string too long!")
+  $client.update(text)
 end
 
+# And here we tweet!
+# tweet!(example_string)
 
+def retweet_all
+  $client.search("to:startupinator", :result_type => "recent").take(1).collect do |tweet|
+    # === DISPLAY THE USERNAME AND THE TWEET RECEIVED
+    # p "You received a tweet from #{tweet.user.screen_name}: #{tweet.text}"
+    # === RETWEET TO THE USER ===
+    # $client.update("@#{tweet.user.screen_name} You're so cool man.")
+  end
+end
 
+retweet_all
 
